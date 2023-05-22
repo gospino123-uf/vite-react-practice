08331474115025aee58eb548e61c047f6ac28dd6
Will Ospino <97458073+gospino123-uf@users.noreply.github.com>
2023-05-22 16:27:09 -0400

    Initialize Vite/React Boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      {/* <h1>Vite &sol React Boilerplate</h1> */}
+      <h1>Boilerplate</h1>
+      <div className="card">
+        <p>Content</p>
+      </div>
+    </>
+  )
+}
+
+export default App
