3f022c6df57d9ce56b55f313ed514c8a0a9e2880
Will Ospino <97458073+gospino123-uf@users.noreply.github.com>
2023-06-15 10:56:58 -0400

    Fix(main): Return App.jsx to boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import './App.css'
 
 function App() {
-    const API_KEY = "";
-    const URL = "https://api.thecatapi.com/v1/";
-    return (
-      <>
-        {/* <h1>Vite &sol React Boilerplate</h1> */}
-        <h1>Fetch Practice</h1>
-        <div className="card">
-          <p>Content</p>
-        </div>
-      </>
-    )
+
+  return (
+    <>
+      {/* <h1>Vite &sol React Boilerplate</h1> */}
+      <h1>Boilerplate</h1>
+      <div className="card">
+        <p>Content</p>
+      </div>
+    </>
+  )
 }
 
 export default App
