00c03cd43445662632d2d916bb8972e7ae84d38b
Will Ospino <97458073+gospino123-uf@users.noreply.github.com>
2023-06-15 09:36:35 -0400

    Initialize(fetch-practice): Use base elements w/o api key

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import './App.css'
 
 function App() {
-
-  return (
-    <>
-      {/* <h1>Vite &sol React Boilerplate</h1> */}
-      <h1>Boilerplate</h1>
-      <div className="card">
-        <p>Content</p>
-      </div>
-    </>
-  )
+    const API_KEY = "";
+    const URL = "https://api.thecatapi.com/v1/";
+    return (
+      <>
+        {/* <h1>Vite &sol React Boilerplate</h1> */}
+        <h1>Fetch Practice</h1>
+        <div className="card">
+          <p>Content</p>
+        </div>
+      </>
+    )
 }
 
 export default App
